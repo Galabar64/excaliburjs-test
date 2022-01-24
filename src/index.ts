0e80f958b62fc536ac738ae8cfcd79ee51a1c0cf
Maxime Larichelliere <MQACX@users.noreply.github.com>
2022-01-24 15:15:32 -0500

    Update excaliburjs and add dev-tools

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import { DevTool } from "@excaliburjs/dev-tools";
 import { Chess } from "./chess/engines/chess";
 
 const game = new Chess();
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const devtool = new DevTool(game);
+
 game.start().then(() => {
   game.goToScene("level");
 });
