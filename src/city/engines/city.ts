6cce96a36259957b3e75eead8d71d3e3fa384155
Maxime Larichellière <MQACX@users.noreply.github.com>
2022-04-03 21:03:34 -0400

    Added City game

---
+++ src/city/engines/city.ts
@@ -0,0 +1,20 @@
+import { TiledMapResource } from "@excaliburjs/plugin-tiled";
+import { DisplayMode, Engine, Loader } from "excalibur";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import exampleTileMapUrl from "/assets/example-city.json";
+
+export class Platform extends Engine {
+  constructor() {
+    super({ displayMode: DisplayMode.FitScreen });
+  }
+
+  public start() {
+    const tiledMapResource = new TiledMapResource("/assets/example-city.json");
+    const loader = new Loader([tiledMapResource]);
+    return super.start(loader).then(() => {
+      tiledMapResource.addTiledMapToScene(this.currentScene);
+      this.currentScene.camera.zoom = 2;
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { Chess } from "./chess/engines/chess";
+import { Platform } from "./city/engines/city";
 
-export const game = new Chess();
+export const game = new Platform();
 
 game.start().then(() => {
   game.goToScene("level");
